255f2ad99e8bbe67bc7575eb9336e4f7609b41b2
Olga Efimova <olga.efimova070797@gmail.com>
2020-02-26 22:58:20 +0300

    deleted unused files from master branch

--- task_1_2.py
+++
@@ -1,4 +0,0 @@
-x = 5
-y = -3
-result = (abs(x) - abs(y)) / (1 + abs(x * y))
-print(result)
--- task_1_5.py
+++
@@ -1,6 +0,0 @@
-cathetus_1 = 3
-cathetus_2 = 4
-hypotenuse = (cathetus_1 ** 2 + cathetus_2 ** 2) ** (1/2)
-area_of_triangle = 1/2 * cathetus_1 * cathetus_2
-print(hypotenuse)
-print(area_of_triangle)
