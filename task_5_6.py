09f74814bf3d8b3c3c69ea051ce487b5a500fdaf
Olga Efimova <olga.efimova070797@gmail.com>
2020-03-05 14:22:58 +0300

    change of 5.6

--- task_5_6.py
+++ task_5_6.py
@@ -1,8 +1,17 @@
 list_of_numbers = [1, 2, 5, 6, 8, 7, 9, 10, 11]
 count = 0
-additional_variable = list_of_numbers[0]
-for i in list_of_numbers:
-    if i > additional_variable:
+following_elem = 0
+index_of_following_elem = 2
+previous_elem = 0
+index_of_previous_elem = 0
+n = 1
+for i in list_of_numbers[1:-1]:
+    following_elem = list_of_numbers[index_of_following_elem]
+    previous_elem = list_of_numbers[index_of_previous_elem]
+    if i > following_elem and i > previous_elem:
         count += 1
-    additional_variable = i
+    if i < following_elem and index_of_following_elem == len(list_of_numbers)-1:
+        count += 1
+    index_of_following_elem += 1
+    index_of_previous_elem += 1
 print(count)
