63401fc82abcccc3085900d8b370703aad1edab9
Olga Efimova <olga.efimova070797@gmail.com>
2020-04-15 21:37:01 +0300

    Added hw20

---
+++ restaurant/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Order(models.Model):
+    order_date = models.DateField()
+    time_of_order = models.TimeField
+    waiter = models.ForeignKey('Waiter', on_delete=models.CASCADE)
+    order_price = models.DecimalField(max_digits=10, decimal_places=2)
+    service_quality_assessment = models.IntegerField
+
+
+class Waiter(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    work_experience = models.DecimalField(max_digits=4, decimal_places=1)
+
+
+class Dish(models.Model):
+    dish = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    weight_grams = models.IntegerField()
+    orders = models.ManyToManyField(Order)
+
+
+class Foodstuff(models.Model):
+    name_of_foodstuff = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    dishes = models.ManyToManyField(Dish)
+
+
+class Delivery(models.Model):
+    delivery_date = models.DateField()
+    supplier = models.CharField(max_length=100)
+    cost_of_delivery = models.DecimalField(max_digits=10, decimal_places=2)
+    delivery_on_time = models.BooleanField()
+    foodstuffs = models.ManyToManyField(Foodstuff)
+
